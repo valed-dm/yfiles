a720b99802e45215b83ab7b67a3db3f7893e47c2
valed-dm <dmvaled@gmail.com>
2024-10-29 19:36:19 +0300

    obtain_url.py created;
    Obtains fresh file download url from Yandex Disk API.

---
+++ yfiles/yd_files/utils/obtain_url.py
@@ -0,0 +1,38 @@
+import logging
+from http import HTTPStatus
+
+from yfiles.yd_files.utils.timeout_requests import TimeoutRequest
+
+logger = logging.getLogger("yfiles")
+
+
+def obtain_fresh_file_url_from_yandex_disk(public_link: str, path: str):
+    api_url = "https://cloud-api.yandex.net/v1/disk/public/resources/download"
+    params = {"public_key": public_link, "path": path}
+
+    connect_timeout = 1
+    read_timeout = 5
+
+    timeout_request = TimeoutRequest(total_timeout=10)
+    response = timeout_request.get(
+        api_url,
+        params=params,
+        timeout=(connect_timeout, read_timeout),
+    )
+
+    if response:
+        if response.status_code == HTTPStatus.OK:
+            return response.json()["href"], response.elapsed.total_seconds()
+
+        download_error_message = (
+            f"{public_link}{path} download failed with "
+            f"status code {response.status_code}"
+        )
+        logger.error(download_error_message)
+        return path, download_error_message
+
+    download_error_message = (
+        f"{public_link}{path} download failed with no response (likely a timeout)"
+    )
+    logger.error(download_error_message)
+    return path, download_error_message

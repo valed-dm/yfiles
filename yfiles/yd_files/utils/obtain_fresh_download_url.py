d12ab78e5b466e8519d6f6c5c1a096f736a4561c
valed-dm <dmvaled@gmail.com>
2024-11-01 01:02:41 +0300

    obtain_fresh_file_download_url() got file_id arg
    for correct mapping pool.imap_unordered() request results with user provided choice (files_to_download).

--- yfiles/yd_files/utils/obtain_fresh_download_url.py
+++ yfiles/yd_files/utils/obtain_fresh_download_url.py
@@ -6,7 +6,7 @@ from yfiles.yd_files.utils.timeout_requests import TimeoutRequest
 logger = logging.getLogger("yfiles")
 
 
-def obtain_fresh_file_download_url(public_link: str, path: str):
+def obtain_fresh_file_download_url(public_link: str, path: str, file_id: int):
     api_url = "https://cloud-api.yandex.net/v1/disk/public/resources/download"
     params = {"public_key": public_link, "path": path}
 
@@ -22,17 +22,17 @@ def obtain_fresh_file_download_url(public_link: str, path: str):
 
     if response:
         if response.status_code == HTTPStatus.OK:
-            return response.json()["href"], response.elapsed.total_seconds()
+            return response.json()["href"], response.elapsed.total_seconds(), file_id
 
         download_error_message = (
             f"{public_link}{path} download failed with "
             f"status code {response.status_code}"
         )
         logger.error(download_error_message)
-        return path, download_error_message
+        return path, download_error_message, file_id
 
     download_error_message = (
         f"{public_link}{path} download failed with no response (likely a timeout)"
     )
     logger.error(download_error_message)
-    return path, download_error_message
+    return path, download_error_message, file_id

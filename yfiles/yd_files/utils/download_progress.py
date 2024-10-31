6c74299e43539606bc3fa3c3126ad4590420fd57
valed-dm <dmvaled@gmail.com>
2024-10-31 19:48:33 +0300

    download_progress.py created
    not in use at the moment

---
+++ yfiles/yd_files/utils/download_progress.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def print_progress_bar(current, total, bar_length=50):
+    progress = current / total
+    percent = progress * 100
+    blocks = int(round(bar_length * progress))
+    bar = "█" * blocks + "-" * (bar_length - blocks)
+
+    # Use sys.stdout.write to overwrite the same line
+    sys.stdout.write(f"\r|{bar}| {percent:.2f}% Complete")
+    sys.stdout.flush()  # Force the system to write immediately

a432d094635600213157649eca10783389046fbf
valed-dm <dmvaled@gmail.com>
2024-11-01 19:29:22 +0300

    save_file_to_disk.py is documented.

--- yfiles/yd_files/utils/save_file_to_disk.py
+++ yfiles/yd_files/utils/save_file_to_disk.py
@@ -4,7 +4,21 @@ from pathlib import Path
 logger = logging.getLogger("yfiles")
 
 
-def save_to_disk(content, filename, directory="YD_Down"):
+def save_to_disk(
+    content: bytes, filename: str, directory: str = "YD_Down",
+) -> Path | None:
+    """
+    Saves binary content to a specified directory on disk.
+
+    Args:
+        content (bytes): The binary content to write to disk.
+        filename (str): The name of the file to be created.
+        directory (str, optional): The directory to save the file in.
+        Defaults to "YD_Down".
+
+    Returns:
+        Optional[Path]: The full path of the saved file if successful, else None.
+    """
     # Ensure the directory exists
     Path(directory).mkdir(parents=True, exist_ok=True)
 

67c978fe4223cdb4defb704da4db39865eca57da
valed-dm <dmvaled@gmail.com>
2024-11-01 19:14:45 +0300

    generate_filename.py is documented and
    base_name identifying string refactored.

--- yfiles/yd_files/utils/generate_filename.py
+++ yfiles/yd_files/utils/generate_filename.py
@@ -5,17 +5,26 @@ from urllib.parse import urlparse
 
 
 def generate_filename_from_download_url(url):
+    """
+    Generates a file name based on the download URL, using available query parameters
+    or the URL path if no filename is specified in the query.
+
+    Args:
+        url (str): The URL from which the file will be downloaded.
+
+    Returns:
+        str: A generated filename with the appropriate extension.
+    """
     # Parse the Yandex download URL to get query parameters and path
     parsed_url = urlparse(url)
     path = parsed_url.path
     query_params = parse_qs(parsed_url.query)
 
-    # Check for a 'filename' parameter and use it if available
-    if "filename" in query_params:
-        base_name = query_params["filename"][0]
-    else:
-        # Fallback to the last part of the path or a generic name if unavailable
-        base_name = path.split("/")[-1] or f"file_{int(time.time())}"
+    # Determine filename using 'filename' parameter if available, otherwise fallback
+    base_name = query_params.get(
+        "filename",
+        [path.split("/")[-1] or f"file_{int(time.time())}"],
+    )[0]
 
     # Get extension or default to .bin
     extension = Path(base_name).suffix or ".bin"

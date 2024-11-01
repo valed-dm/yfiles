9ab899f3c4251b854a1ece105356afd3f5157e00
valed-dm <dmvaled@gmail.com>
2024-11-01 19:58:53 +0300

    tasks.py is documented.

--- yfiles/yd_files/tasks.py
+++ yfiles/yd_files/tasks.py
@@ -9,9 +9,22 @@ logger = logging.getLogger("yfiles")
 
 
 @shared_task(soft_time_limit=300, time_limit=360)
-def download_file_in_background(url, filename, directory="YD_Down"):
-    """Downloads a file from the provided URL and saves it
-    to the specified directory.
+def download_file_in_background(
+    url: str, filename: str, directory: str = "YD_Down",
+) -> str | None:
+    """
+    Downloads a file from the provided URL and saves it to the specified directory.
+    Tracks download progress in increments of 20% and logs completion or failure.
+
+    Args:
+        url (str): The URL from which to download the file.
+        filename (str): The name under which the file will be saved.
+        directory (str, optional): The directory to save the file.
+        Defaults to "YD_Down".
+
+    Returns:
+        Optional[str]: A success message with the file path if successful,
+        or an error message if the download fails.
     """
     timeout_request = TimeoutRequest(total_timeout=300)
 
@@ -33,12 +46,13 @@ def download_file_in_background(url, filename, directory="YD_Down"):
                     current_size = file.tell()
                     percent = (current_size / total_size) * 100
                     if percent >= next_log_threshold:
-                        # Log the progress in 10% increments
-                        msg = (
-                            f"Download progress for {filename}:"
-                            f" {percent:.2f}% Complete, total_size={total_size}"
+                        # Log the progress in 20% increments
+                        logger.info(
+                            "Download progress for %s: %.2f%% Complete, total_size=%d",
+                            filename,
+                            percent,
+                            total_size,
                         )
-                        logger.info(msg)
                         next_log_threshold += 20  # Increment the threshold
 
         logger.info("File downloaded and saved to %s", file_path)

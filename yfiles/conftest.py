2123a0e2d72147ddaf3b1add767a88b7700d3924
valed-dm <dmvaled@gmail.com>
2024-11-07 18:41:32 +0300

    conftest.py got STATICFILES_DIRS fixture

--- yfiles/conftest.py
+++ yfiles/conftest.py
@@ -9,6 +9,12 @@ def _media_storage(settings, tmpdir) -> None:
     settings.MEDIA_ROOT = tmpdir.strpath
 
 
+@pytest.fixture(autouse=True)
+def _static_files(settings, tmpdir) -> None:
+    settings.STATICFILES_DIRS = []
+    settings.STATIC_ROOT = tmpdir.mkdir("staticfiles")
+
+
 @pytest.fixture
 def user(db) -> User:
     return UserFactory()

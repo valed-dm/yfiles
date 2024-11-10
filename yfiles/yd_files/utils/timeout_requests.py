6fdf243dc3a8187fb8c89a472ef0625ba124adb2
valed-dm <dmvaled@gmail.com>
2024-11-11 00:14:05 +0300

    timeout_requests revised
    for separation timeout handler from request logic (raise TimeoutError(msg)).

--- yfiles/yd_files/utils/timeout_requests.py
+++ yfiles/yd_files/utils/timeout_requests.py
@@ -37,22 +37,26 @@ class TimeoutRequest:
 
     def _timeout_handler(self):
         """
-        Waits for the total timeout duration, and if exceeded, logs an error,
-        sets the timeout flag, and raises a TimeoutError.
+        Monitors the total timeout duration for the request.
+
+        If the request does not complete within the specified timeout,
+        it sets the `timed_out` flag to True and logs an error message.
+        This function is run in a separate thread to allow the main request
+        to proceed while monitoring the total timeout.
         """
-        if not self.timeout_event.wait(
-            self.total_timeout,
-        ):
+        if not self.timeout_event.wait(self.total_timeout):
             self.timed_out = True
-            timeout_message = (
-                f"Request exceeded total timeout of {self.total_timeout} seconds."
-            )
-            logger.error(timeout_message)
-            raise TimeoutError(timeout_message)
+            msg = f"Request exceeded total timeout of {self.total_timeout} seconds."
+            logger.error(msg)
 
     def get(self, url, **kwargs) -> Response | None:
         """
         Sends a GET request to the specified URL, with a monitored timeout period.
+
+         This method starts a separate thread that monitors the total timeout period.
+        If the request completes within the allowed time, the response is returned.
+        Otherwise, a `TimeoutError` is raised. Any other exceptions during the request
+        will be logged, and None will be returned.
 
         Args:
             url (str): The URL to send the GET request to.
@@ -61,15 +65,25 @@ class TimeoutRequest:
         Returns:
             Optional[Response]: The HTTP response if successful and within the timeout,
             or None if an error occurs or the request exceeds the timeout.
+
+         Returns:
+            Optional[Response]: The HTTP response if successful and within the timeout,
+            or None if an error occurs or the request exceeds the timeout.
+
+         Raises:
+            TimeoutError: If the request exceeds the specified total timeout.
         """
         timeout_thread = threading.Thread(target=self._timeout_handler, daemon=True)
         timeout_thread.start()
 
         try:
             response = requests.get(url, **kwargs)  # noqa: S113
-            if not self.timed_out:  # Check if the request finished within the timeout
-                self.timeout_event.set()  # Signal completion to the timeout handler
-                return response
+            if self.timed_out:
+                msg = f"Request exceeded total timeout of {self.total_timeout} seconds."
+                raise TimeoutError(msg)
+            self.timeout_event.set()  # Signal completion to the timeout handler
+            return response  # noqa: TRY300
+
         except requests.exceptions.RequestException as e:
             msg = f"Request error: {e}"
             logger.exception(msg)
